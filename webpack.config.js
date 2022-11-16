faa0ab12be31a44e0e3d830bb516f612cec1955b
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2022-11-16 16:44:21 +0100

    Webpack config multiple js files
    Updated the webpack config for multiple JS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    //   entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        encyclopedia: './src/encyclopedia.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'docs'),
     },
     mode: 'production'
