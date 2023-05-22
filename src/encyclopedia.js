9f07dd4426da522590b8e045d6453f93b9c26aba
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-22 22:36:46 +0200

    Encyclopedia overlay switch
    Added the Encyclopedia overlay toggle

--- src/encyclopedia.js
+++ src/encyclopedia.js
@@ -1,7 +1,29 @@
-console.log('webpack test 4');
+window.onload = (event) => {
 
-const overlay = document.getElementById("overlay")
+const { over } = require("lodash");
 
-document.getElementById("g").addEventListener("click", () => {
-    overlay.style.display = 'block';
-});
+var overlay = document.getElementById("overlay")
+var nonOverlay = document.getElementById("monster_page");
+var openOverlay = document.getElementsByClassName("monsterName")
+var openOverlay2 = document.getElementsByClassName("monsterIcon")
+
+
+function on(event) {
+    if (Array.from(openOverlay).some(element => element === event.target) || Array.from(openOverlay2).some(element => element === event.target)) {
+        overlay.style.display = 'block';
+        console.log("open overlay")
+    }
+}
+
+function off(event) {   
+    if (event.target == overlay && !nonOverlay.contains(event.target)) {
+        overlay.style.display = 'none';
+        console.log("close overlay")
+    }    
+}
+
+document.addEventListener("click", on)
+document.addEventListener("click", off);
+
+}
+
