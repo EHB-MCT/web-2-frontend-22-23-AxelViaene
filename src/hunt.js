1e3cb32e06d8031c984a25664a907031fb51af5f
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-23 23:31:00 +0200

    WIP hunt overlay
    CSS, HTML and Javascript for overlay on hunt page

--- src/hunt.js
+++ src/hunt.js
@@ -1,6 +1,30 @@
+window.onload = (event) => {
 console.log('loaded')
 function selectRegion() {
     document.getElementById('regionDropdown').classList.toggle("show");
 }
 
 document.getElementById('regionDropdown').onclick = selectRegion()
+
+var overlay = document.getElementById("overlay")
+var openOverlay = document.getElementById("huntButton")
+var nonOverlay = document.getElementsByClassName("huntOverlay");
+
+function on(event) {
+    if (event.target === openOverlay) {
+        overlay.style.display = 'block';
+        console.log("open overlay")
+    }
+}
+
+function off(event) {   
+    if (event.target == overlay && !nonOverlay.contains(event.target)) {
+        overlay.style.display = 'none';
+        console.log("close overlay")
+    }    
+}
+
+document.addEventListener("click", on)
+document.addEventListener("click", off);
+}
+
