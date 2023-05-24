d8d97184226bd284cf99988ca36bf19289cdf116
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-24 13:23:03 +0200

    Hunt carousel
    Basic HTML, CSS & JS for a carousel in hunt

--- src/hunt.js
+++ src/hunt.js
@@ -1,14 +1,58 @@
 window.onload = (event) => {
 console.log('loaded')
-function selectRegion() {
-    document.getElementById('regionDropdown').classList.toggle("show");
+
+var dropdown = document.getElementById('regionDropdown');
+var regionButton = document.getElementById('regionButton');
+var overlay = document.getElementById("overlay");
+var openOverlay = document.getElementById("huntButton");
+var nonOverlay = document.getElementById("huntOverlay");
+
+
+//Carousel//
+
+var carousel = document.querySelector('.carousel');
+var slides = carousel.getElementsByClassName('slide');
+var currentIndex = 0;
+var previousButton = document.getElementById("PreviousWeapon");
+var nextButton = document.getElementById("nextWeapon");
+
+function showWeapon(index) {
+    for (var i =0; i < slides.length; i++) {
+        slides[i].classList.remove('activeWeapon');
+    }
+    console.log('showing')
+    slides[index].classList.add('activeWeapon');
 }
 
-document.getElementById('regionDropdown').onclick = selectRegion()
+function nextWeapon() {
+    currentIndex++;
+    if(currentIndex >= slides.length) {
+        currentIndex = 0
+    }
+    showWeapon(currentIndex);
+}
 
-var overlay = document.getElementById("overlay")
-var openOverlay = document.getElementById("huntButton")
-var nonOverlay = document.getElementsByClassName("huntOverlay");
+function previousWeapon() {
+    currentIndex--;
+    if(currentIndex < 0) {
+        currentIndex = slides.length -1;
+    }
+    showWeapon(currentIndex);
+}
+
+function toggleDropdownOn(event) {
+    if (event.target === regionButton ) {
+        dropdown.style.display = 'block';
+        console.log("Select region open");        
+    }
+}
+
+function toggleDropdownOff(event) {
+    if (event.target !== regionButton) {
+        dropdown.style.display = 'none';
+        console.log("Select region close");
+    }
+}
 
 function on(event) {
     if (event.target === openOverlay) {
@@ -24,7 +68,11 @@ function off(event) {
     }    
 }
 
-document.addEventListener("click", on)
+document.addEventListener("click", toggleDropdownOn);
+document.addEventListener("click", toggleDropdownOff);
+document.addEventListener("click", on);
 document.addEventListener("click", off);
+previousButton.addEventListener('click', previousWeapon);
+nextButton.addEventListener('click', nextWeapon);
 }
 
