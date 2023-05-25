2c48e09ec8132e5a0d1868fe31b5dc7c1a81d6b5
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-25 16:41:51 +0200

    Frontend - backend testing
    Changed code for further testing between backend and frontend

--- src/index.js
+++ src/index.js
@@ -8,10 +8,17 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
+
 component();
 
-fetch('http://localhost:3000/users/').then(response=> response.json()).then(data => {
+fetch('http://localhost:3000/users').then(response=> response.json()).then(data => {
     console.log(data);
+    console.log('test')
+    // console.log(`UserId is: ` + data[1].UserId);
+    // const UserId = data[1].UserId
+    // console.log(`GreatswordId is: ` + data[1].GreatswordId);
+    // const GreatswordId = data[1].GreatswordId
+   
 })
 }
 
