f8eae36ef3a25aeea9496d05cd35d72054051e8d
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-25 12:44:27 +0200

    Frontend-backend connection test
    With dummy data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
+window.onload =() => {
 
 function component() {
     const element = document.createElement('div');
-
+    console.log("Hello")
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
     return element;
 }
+component();
 
-document.body.appendChild(component());
+fetch('http://localhost:3000/users/').then(response=> response.json()).then(data => {
+    console.log(data);
+})
+}
+
+
