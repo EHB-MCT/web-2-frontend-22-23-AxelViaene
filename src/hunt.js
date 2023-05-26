85e2af16dce6be7f65befee95e2a30f209a6cbac
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-26 22:02:19 +0200

    CSS, HTML and JS updates
    General CSS & HTML updates
    Added functionality in Hunt with carousel displaying number, monster rank showing selected rank and displayed selected region

--- src/hunt.js
+++ src/hunt.js
@@ -3,33 +3,38 @@ console.log('load')
 
 document.addEventListener("click", toggleDropdownOn);
 document.addEventListener("click", toggleDropdownOff);
-document.addEventListener("click", on);
-document.addEventListener("click", off);
+document.addEventListener("click", huntResultOverlay);
+document.addEventListener("click", huntResultClose);
 previousButton.addEventListener('click', previousWeapon);
 nextButton.addEventListener('click', nextWeapon);
 }
 
+//Region select
 var dropdown = document.getElementById('regionDropdown');
 var regionButton = document.getElementById('regionButton');
+var regions = document.getElementsByClassName('regionOption')
 var overlay = document.getElementById("overlay");
 var openOverlay = document.getElementById("huntButton");
 var nonOverlay = document.getElementById("huntOverlay");
 
  
 //Carousel//
-
 var carousel = document.querySelector('.carousel');
 var slides = carousel.getElementsByClassName('slide');
 var currentIndex = 0;
-var previousButton = document.getElementById("PreviousWeapon");
+var previousButton = document.getElementById("previousWeapon");
 var nextButton = document.getElementById("nextWeapon");
+var carouselNumber = document.querySelector('.carouselNumber');
 
+
+//carousel
 function showWeapon(index) {
     for (var i =0; i < slides.length; i++) {
         slides[i].classList.remove('activeWeapon');
     }
     console.log('showing')
     slides[index].classList.add('activeWeapon');
+    carouselNumber.textContent = (index + 1) + "/" + slides.length;
 }
 
 function nextWeapon() {
@@ -48,31 +53,56 @@ function previousWeapon() {
     showWeapon(currentIndex);
 }
 
+
+//region dropdown
 function toggleDropdownOn(event) {
     if (event.target === regionButton ) {
         dropdown.style.display = 'block';
-        console.log("Select region open");        
+    }
+    if (event.target.classList.contains('regionOption')) {
+        var selectedRegion = event.target.textContent;
+        regionButton.textContent = selectedRegion;
+        dropdown.style.display = 'none';
     }
 }
 
 function toggleDropdownOff(event) {
     if (event.target !== regionButton) {
-        dropdown.style.display = 'none';
-        console.log("Select region close");
+        dropdown.style.display = 'none';  
     }
 }
 
-function on(event) {
+//hunt overlay
+function huntResultOverlay(event) {
     if (event.target === openOverlay) {
         overlay.style.display = 'block';
         console.log("open overlay")
     }
 }
 
-function off(event) {   
+function huntResultClose(event) {   
     if (event.target == overlay && !nonOverlay.contains(event.target)) {
         overlay.style.display = 'none';
         console.log("close overlay")
     }    
 }
 
+//Monster rank select
+var rankIcons = document.querySelectorAll('.rankIcon');
+var monsterRankSelected = -1;
+
+rankIcons.forEach(function(icon, index) {
+    icon.addEventListener('click', function() {
+     
+      rankIcons.forEach(function(rank, i) {
+        if (i <= index) {
+          rank.src = '../icons/star-full.png';
+        } else {
+          rank.src = '../icons/star-empty.png';
+        }
+      });
+  
+      monsterRankSelected = index;
+     
+    });
+  });
