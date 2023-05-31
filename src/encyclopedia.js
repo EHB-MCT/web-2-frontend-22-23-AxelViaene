950efff1393fe2306b655f237fcc992e5cd48fb0
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-05-31 16:09:11 +0200

    Encyclopedia stuck testing overlay combination

--- src/encyclopedia.js
+++ src/encyclopedia.js
@@ -35,72 +35,57 @@ var allMonsters = link.map(function(result) {
     return result.MonsterId
 })
 var uniqueMonsters = new Set(allMonsters)
-console.log(uniqueMonsters)
+// console.log(uniqueMonsters)
 
 var HTMLmain = document.getElementById('main');
-var overlayHTML = document.getElementById('overlay');
 var monstercounter = 1
 
 //displaying the amount of monsters in user collection
 uniqueMonsters.forEach(function(monster) {
     fetch(`https://mhw-db.com/monsters/${monster}`).then(response => 
         response.json()).then(apidata=> {
-            console.log(apidata)
-            console.log(apidata.name)
+            // console.log(apidata)
+            // console.log(apidata.name)
 
 
             let htmldata = 
             `<div class="monster monster${monstercounter}">
             <div class="monsterInternal">
-              <img class="monsterIcon" src="../icons/monsters/${apidata.name}.png" alt="">
-              <p class="monsterName" id="monstername${monstercounter}">${apidata.name}</p>
+              <img id="monsterIcon${monstercounter}" src="../icons/monsters/${apidata.name}.png" alt="">
+              <p id="monsterName${monstercounter}">${apidata.name}</p>
             </div>
             </div>`;
 
-            let overlaydata = `<div id="monster_page${monstercounter}">
-            <div class="overlay_left">
-              <p class="overlay_monster_species">Flying Wyvern</p>
-              <p class="overlay_monster_name">${apidata.name}</p>
-              <img class="overlay_img" src="../icons/monsters/Behemoth.png" alt="">
-            </div>
-            <div class="overlay_right">
-              <p class="overlay_known_regions">Known Regions</p>
-              <p class="overlay_regions">Ancient Forest</p>
-              <p class="overlay_regions">Ancient Forest</p>
-              <p class="overlay_regions">Ancient Forest</p>
-              <p class="overlay_regions">Ancient Forest</p>
-              <p class="overlay_regions">Ancient Forest</p>
-              <p class="overlay_characteristics">Characteristics</p>
-              <p class="overlay_description">A nefarious flying wyvern that travels the New World in search of prey. It
-                scatters explosive scales over a wide area to prey on whatever gets caught in the blast.</p>
-              <div class="overlay_weaknesses">
-                <p>Weaknesses</p>
-                <img class="overlay_weaknesses_icon" src="../icons/elements/Dragon.png" alt="">
-                <img class="overlay_weaknesses_icon" src="../icons/elements/Poison.png" alt="">
-                <img class="overlay_weaknesses_icon" src="../icons/elements/Water.png" alt="">
-              </div>
-              <div class="overlay_resistances">
-                <p>Resistances</p>
-                <img class="overlay_resistances_icon" src="../icons/elements/Fire.png" alt="">
-              </div>
-            </div>
-          </div>`
-
             HTMLmain.insertAdjacentHTML('beforeend', htmldata);
-            overlayHTML.insertAdjacentHTML('beforeend', overlaydata)
             monstercounter++;
         })
     })
-})  
+})
 
    //overlay
    var overlay = document.getElementById("overlay")
-   var openOverlay = document.getElementsByClassName("monsterName")
-   var openOverlay2 = document.getElementsByClassName("monsterIcon")
+   var openOverlay = document.querySelectorAll("#monsterName1, #monstername2")
+   var openOverlay2 = document.querySelectorAll("#monsterIcon1, #monsterIcon2")
+   console.log(openOverlay)
+//    var monsterSections = document.querySelectorAll('.monsterIcon, .monsterName');
+//    console.log(Array.from(monsterSections))
+
+   //saving specific clicked monster
+   monsterSections.forEach(function(section) {
+    section.addEventListener('click', function(event) {
+        var clickedElement = event.target;
+        if(clickedElement.classList.contains('monsterIcon') || clickedElement.classList.contains('monsterName')) {
+            var monsterCard = section;
+            console.log(monsterCard)
+        }
+    })
+   })
    
    function on(event) {
        if (Array.from(openOverlay).some(element => element === event.target) || Array.from(openOverlay2).some(element => element === event.target)) {
-           overlay.style.display = 'block';
+        // openOverlay.parentElement.style.display = 'flex'
+        
+        overlay.style.display = 'block';
        }
    }
    
